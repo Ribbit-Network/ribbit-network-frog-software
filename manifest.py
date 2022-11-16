3d1d8655868e276410852fb3cb3a74583a929645
Damien Tournoud <damien@platform.sh>
2022-11-16 09:43:29 -0800

    manifest.py: Embed current year in the firmware
    That will allow us to check if timestamps are plausible.

--- manifest.py
+++ manifest.py
@@ -17,10 +17,12 @@ commit_id = subprocess.check_output(
     ["git", "rev-parse", "HEAD"],
     encoding="utf-8",
 )
+now = datetime.datetime.utcnow()
 
 with open("__version__.py", "w", encoding="utf-8") as f:
     f.write("version = %r\n" % version.strip())
     f.write("commit_id = %r\n" % commit_id.strip())
-    f.write("build_date = %r\n" % datetime.datetime.utcnow().isoformat())
+    f.write("build_date = %r\n" % now.isoformat())
+    f.write("build_year = %d\n" % now.year)
 
 module("__version__.py")
