c32894c8e30a6adc957f6ed2f2c618e82683565c
Damien Tournoud <damien@platform.sh>
2022-12-02 16:10:22 -0800

    .github: Upload release firmware to Golioth

---
+++ tools/upload-to-golioth.py
@@ -0,0 +1,59 @@
+import base64
+import json
+import os
+
+import requests
+
+
+def load_version():
+    with open("__version__.py") as f:
+        data = f.read()
+
+    ret = {}
+    exec(data, None, ret)
+    return ret
+
+
+version_data = load_version()
+
+session = requests.Session()
+session.headers["x-api-key"] = os.environ["GOLIOTH_API_KEY"]
+
+project = os.environ["GOLIOTH_PROJECT"]
+blueprint = os.environ["GOLIOTH_BLUEPRINT"]
+
+req = {
+    "blueprintId": blueprint,
+    "package": "main",
+    "projectId": project,
+    "version": version_data["version"],
+}
+
+with open("firmware/micropython.bin", "rb") as f:
+    req["content"] = base64.b64encode(f.read()).decode("ascii")
+
+r = session.post(
+    "https://api.golioth.io/v1/artifacts",
+    data=json.dumps(req),
+    headers={
+        "Content-Type": "application/json",
+    },
+)
+r.raise_for_status()
+artifact = r.json()
+
+r = session.post(
+    "https://api.golioth.io/v1/projects/%s/releases" % (project,),
+    data=json.dumps(
+        {
+            "blueprintId": blueprint,
+            "artifactIds": [
+                artifact["data"]["id"],
+            ],
+        }
+    ),
+    headers={
+        "Content-Type": "application/json",
+    },
+)
+r.raise_for_status()
